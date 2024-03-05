2f75bcc5dfac864f2323f279a754e7cd48a83a34
Sachinqspider <162303673+Sachinqspider@users.noreply.github.com>
2024-03-05 15:44:28 +0530

    Update Demo.java

--- DemoGit/src/Demo.java
+++ DemoGit/src/Demo.java
@@ -3,7 +3,7 @@ public class Demo {
 
 	public static void main(String[] args) {
 		System.out.println("Hello world");
-		System.out.println("Bye World");
+		System.out.println("Hii World");
 	}
 
 }
